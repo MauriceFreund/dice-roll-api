8f55641a1eb764a6d25966436c79ca9c711792bb
Maurice Freund <maurice.freund009@stud.fh-dortmund.de>
2021-01-23 21:45:53 +0100

    Init commit for dice-roll-api

---
+++ src/main/java/com/mauricefreund/dicerollapi/DiceRollApiApplication.java
@@ -0,0 +1,13 @@
+package com.mauricefreund.dicerollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceRollApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceRollApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauricefreund/dicerollapi/DiceRollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricefreund.dicerollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceRollApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
