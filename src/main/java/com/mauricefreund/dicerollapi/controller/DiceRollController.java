27d2b375f633307005885ccb015a25903adc2cc1
Maurice Freund <maurice.freund009@stud.fh-dortmund.de>
2021-01-23 23:27:53 +0100

    Create DiceRollController.java
    Added DiceRollController

---
+++ src/main/java/com/mauricefreund/dicerollapi/controller/DiceRollController.java
@@ -0,0 +1,25 @@
+package com.mauricefreund.dicerollapi.controller;
+
+import com.mauricefreund.dicerollapi.services.DiceRollService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin()
+public class DiceRollController {
+
+    private final DiceRollService rollService;
+
+    @Autowired
+    public DiceRollController(DiceRollService rollService) {
+        this.rollService = rollService;
+    }
+
+    @GetMapping("/rolldice")
+    public double calculateResultForDiceRoll(@RequestParam(value="roll", defaultValue = "1d6") String rollDescription) {
+        return this.rollService.generateResultForDiceRoll(rollDescription);
+    }
+}
