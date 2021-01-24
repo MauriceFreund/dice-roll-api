ef96ffdd0558e7599cb30e85e7021e8899c22534
Maurice Freund <maurice.freund009@stud.fh-dortmund.de>
2021-01-25 00:00:52 +0100

    Allowed CORS from all sources in DiceRollController

--- src/main/java/com/mauricefreund/dicerollapi/controller/DiceRollController.java
+++ src/main/java/com/mauricefreund/dicerollapi/controller/DiceRollController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin()
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DiceRollController {
 
     private final DiceRollService rollService;
